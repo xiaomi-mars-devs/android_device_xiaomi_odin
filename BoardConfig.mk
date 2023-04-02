6c51065008f389b425c3416942e87be92da001c8
FlowerSea0208 <zzp060208@gmail.com>
2023-04-02 18:42:56 +0800

    odin: Call the proprietary setup
    Change-Id: I71f2973e7ded6a250aa8912294c3c2bc7967f4a1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,3 +8,6 @@ DEVICE_PATH := device/xiaomi/odin
 
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
+
+# Include proprietary files
+include vendor/xiaomi/odin/BoardConfigVendor.mk
